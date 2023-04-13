56d38f3e242ce30ad6fe868eae253fa4427315a0
Abubakar Siddiqui <abubakarsiddiqui866@gmail.com>
2023-04-13 10:19:25 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import python from "./tech/python.png";
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import githubi from "./tech/github.png";
+import django from "./tech/django.png";
+import canva from "./tech/canva.png";
+import bootstrap from "./tech/bootstrap.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+
+import youvah from "./company/youvah.png";
+import americanchase from "./company/americanchase.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  githubi,
+  menu,
+  close,
+  css,
+  python,
+  docker,
+  figma,
+  git,
+  django,
+  canva,
+  bootstrap,
+  html,
+  javascript,
+  reactjs,
+  tailwind,
+  threejs,
+  youvah,
+  americanchase,
+  carrent,
+  jobit,
+  tripguide,
+};
